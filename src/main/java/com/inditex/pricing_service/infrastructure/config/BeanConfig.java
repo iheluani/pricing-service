704a60199cbde437bdba91f411dce7493966addf
iheluani <heluani@hotmail.com>
2026-01-10 09:52:02 +0100

    feat: add use case for applicable price resolution

---
+++ src/main/java/com/inditex/pricing_service/infrastructure/config/BeanConfig.java
@@ -0,0 +1,23 @@
+package com.inditex.pricing_service.infrastructure.config;
+
+import com.inditex.pricing_service.application.port.in.GetApplicablePriceQuery;
+import com.inditex.pricing_service.application.port.out.PriceRepositoryPort;
+import com.inditex.pricing_service.application.service.GetApplicablePriceService;
+import com.inditex.pricing_service.infrastructure.adapter.out.persistence.PriceRepositoryAdapter;
+import com.inditex.pricing_service.infrastructure.adapter.out.persistence.SpringDataPriceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public PriceRepositoryPort priceRepositoryPort(SpringDataPriceRepository springDataPriceRepository) {
+        return new PriceRepositoryAdapter(springDataPriceRepository);
+    }
+
+    @Bean
+    public GetApplicablePriceQuery getApplicablePriceQuery(PriceRepositoryPort priceRepositoryPort) {
+        return new GetApplicablePriceService(priceRepositoryPort);
+    }
+}
