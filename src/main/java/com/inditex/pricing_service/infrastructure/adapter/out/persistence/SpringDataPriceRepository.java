c41985551898a034e254dfeffdeb91473c5c8672
iheluani <heluani@hotmail.com>
2026-01-10 09:51:44 +0100

    feat: implement persistence adapter with efficient price lookup

--- src/main/java/com/inditex/pricing_service/infrastructure/adapter/out/persistence/SpringDataPriceRepository.java
+++ src/main/java/com/inditex/pricing_service/infrastructure/adapter/out/persistence/SpringDataPriceRepository.java
@@ -2,6 +2,15 @@ package com.inditex.pricing_service.infrastructure.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface SpringDataPriceRepository extends JpaRepository<PriceJpaEntity, Long> {
 
+    Optional<PriceJpaEntity> findFirstByBrandIdAndProductIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualOrderByPriorityDescStartDateDesc(
+            Long brandId,
+            Long productId,
+            LocalDateTime applicationDate1,
+            LocalDateTime applicationDate2
+    );
 }
